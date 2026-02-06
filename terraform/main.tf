01dfc669d8d2e62e3192a1e2ecd286fa9a0e8b8b
Zahra Hashmi Khamgaowala <zhashmi386@gmail.com>
2026-02-06 12:34:47 -0500

    Add AWS instances for database, backend, and frontend

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+     region="us-east-1"
+}
+
+resource "aws_instance" "database" {
+     ami="ami-0b6c6ebed2801a5cb"
+     instance_type="t2.medium"
+     vpc_security_group_ids = ["sg-07dd3fbd2230f8dbb"]
+     subnet_id              = subnet-02884077c63e5a6ea
+     key_name               = "user"
+     tags={
+        Name = "Database-jp"
+     }
+}
+resource "aws_instance" "backend" {
+     ami="ami-0b6c6ebed2801a5cb"
+     instance_type="t2.medium"
+     vpc_security_group_ids = ["sg-07dd3fbd2230f8dbb"]
+     subnet_id              = subnet-02884077c63e5a6ea
+     key_name               = "user"
+     tags={
+        Name = "Backend-jp"
+     }
+}
+resource "aws_instance" "frontend" {
+     ami="ami-0b6c6ebed2801a5cb"
+     instance_type="t2.medium"
+     vpc_security_group_ids = ["sg-07dd3fbd2230f8dbb"]
+     subnet_id              = subnet-02884077c63e5a6ea
+     key_name               = "user"
+     tags={
+        Name = "Frontend-jp"
+     }
+}
