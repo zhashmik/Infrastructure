3cf0402917c78f3de0089de7421a31a70147d90b
Zahra Hashmi Khamgaowala <zhashmi386@gmail.com>
2026-02-06 12:45:58 -0500

    add public IP outputs
    Updated subnet_id values to be quoted and added outputs for public IPs.

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "database" {
      ami="ami-0b6c6ebed2801a5cb"
      instance_type="t2.medium"
      vpc_security_group_ids = ["sg-07dd3fbd2230f8dbb"]
-     subnet_id              = subnet-02884077c63e5a6ea
+     subnet_id              = "subnet-02884077c63e5a6ea"
      key_name               = "user"
      tags={
         Name = "Database-jp"
@@ -16,7 +16,7 @@ resource "aws_instance" "backend" {
      ami="ami-0b6c6ebed2801a5cb"
      instance_type="t2.medium"
      vpc_security_group_ids = ["sg-07dd3fbd2230f8dbb"]
-     subnet_id              = subnet-02884077c63e5a6ea
+     subnet_id              = "subnet-02884077c63e5a6ea"
      key_name               = "user"
      tags={
         Name = "Backend-jp"
@@ -26,9 +26,19 @@ resource "aws_instance" "frontend" {
      ami="ami-0b6c6ebed2801a5cb"
      instance_type="t2.medium"
      vpc_security_group_ids = ["sg-07dd3fbd2230f8dbb"]
-     subnet_id              = subnet-02884077c63e5a6ea
+     subnet_id              = "subnet-02884077c63e5a6ea"
      key_name               = "user"
      tags={
         Name = "Frontend-jp"
      }
+}
+
+output "database_public_ip" {
+ value=aws_instance.database.public_ip
+}
+output "frontend_public_ip" {
+ value=aws_instance.frontend.public_ip
+}
+output "backend_public_ip" {
+ value=aws_instance.backend.public_ip
 }
